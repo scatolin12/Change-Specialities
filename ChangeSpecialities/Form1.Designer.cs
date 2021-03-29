87667125272dcfb64fe546c2e7cb80a81a36d15b
Alexandre Scatolin Tem-Pass <74421450+scatolin12@users.noreply.github.com>
2021-03-29 09:59:06 -0300

    First commit Change Specialities

--- ChangeSpecialities/Form1.Designer.cs
+++ ChangeSpecialities/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChangeSpecialities
 {
-    partial class Form1
+    partial class Specialities
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Specialities));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mainmaterial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Component = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.lbl_plant = new System.Windows.Forms.Label();
+            this.tb_plant = new System.Windows.Forms.TextBox();
+            this.btn_new = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.mainmaterial,
+            this.Component,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(549, 631);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown_1);
+            // 
+            // mainmaterial
+            // 
+            this.mainmaterial.HeaderText = "Main Material";
+            this.mainmaterial.Name = "mainmaterial";
+            // 
+            // Component
+            // 
+            this.Component.HeaderText = "Component";
+            this.Component.Name = "Component";
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.Width = 300;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(63, 89);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(75, 23);
+            this.btn_run.TabIndex = 3;
+            this.btn_run.Text = "Run";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Location = new System.Drawing.Point(12, 34);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(40, 13);
+            this.lbl_id.TabIndex = 2;
+            this.lbl_id.Text = "ID item";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(63, 31);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(75, 20);
+            this.tb_id.TabIndex = 1;
+            // 
+            // lbl_plant
+            // 
+            this.lbl_plant.AutoSize = true;
+            this.lbl_plant.Location = new System.Drawing.Point(12, 58);
+            this.lbl_plant.Name = "lbl_plant";
+            this.lbl_plant.Size = new System.Drawing.Size(31, 13);
+            this.lbl_plant.TabIndex = 2;
+            this.lbl_plant.Text = "Plant";
+            // 
+            // tb_plant
+            // 
+            this.tb_plant.Location = new System.Drawing.Point(63, 55);
+            this.tb_plant.Name = "tb_plant";
+            this.tb_plant.Size = new System.Drawing.Size(75, 20);
+            this.tb_plant.TabIndex = 2;
+            // 
+            // btn_new
+            // 
+            this.btn_new.Location = new System.Drawing.Point(502, 24);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(41, 23);
+            this.btn_new.TabIndex = 5;
+            this.btn_new.Text = "New";
+            this.btn_new.UseVisualStyleBackColor = true;
+            this.btn_new.Click += new System.EventHandler(this.btn_new_Click);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "Export";
+            this.button1.AccessibleName = "Export";
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(502, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 33);
+            this.button1.TabIndex = 7;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Export";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 769);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(236, 769);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 10;
+            // 
+            // Specialities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(575, 791);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_new);
+            this.Controls.Add(this.tb_plant);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.lbl_plant);
+            this.Controls.Add(this.lbl_id);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Specialities";
+            this.Text = "Change Specialities";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mainmaterial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Component;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label lbl_plant;
+        private System.Windows.Forms.TextBox tb_plant;
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- ChangeSpecialities/Form1.cs
+++ ChangeSpecialities/Form1.cs
@@ -7,14 +7,292 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SAPFEWSELib;
+using SapROTWr;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ChangeSpecialities
 {
-    public partial class Form1 : Form
+    public partial class Specialities : Form
     {
-        public Form1()
+        public Specialities()
         {
             InitializeComponent();
         }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            if (tb_id.Text == "" || tb_plant.Text == "")
+            {
+                return;
+            }
+            if (tb_id.TextLength > 4)
+            {
+                MessageBox.Show("ID item not valid", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (tb_plant.TextLength != 4)
+            {
+                MessageBox.Show("Centro inválido", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+
+            //INICIAR ALTERAÇÃO
+
+            #region
+            if (dataGridView1.RowCount == 1)
+            {
+                return;
+            }
+
+            try
+            {
+
+
+                //Get the Windows Running Object Table
+                SapROTWr.CSapROTWrapper sapROTWrapper = new SapROTWr.CSapROTWrapper();
+                //Get the ROT Entry for the SAP Gui to connect to the COM
+                object SapGuilRot = sapROTWrapper.GetROTEntry("SAPGUI");
+                //Get the reference to the Scripting Engine
+                object engine = SapGuilRot.GetType().InvokeMember("GetScriptingEngine", System.Reflection.BindingFlags.InvokeMethod, null, SapGuilRot, null);
+                //Get the reference to the running SAP Application Window
+                GuiApplication GuiApp = (GuiApplication)engine;
+                //Get the reference to the first open connection
+                GuiConnection connection = (GuiConnection)GuiApp.Connections.ElementAt(0);
+                //get the first available session
+                GuiSession session = (GuiSession)connection.Children.ElementAt(0);
+                //Get the reference to the main "Frame" in which to send virtual key commandss
+                //GuiFrameWindow frame = session.ActiveWindow;
+                GuiFrameWindow frame = (GuiFrameWindow)session.FindById("wnd[0]");
+                //((GuiFrameWindow)frame.FindById("wnd[0]"));
+                ((GuiOkCodeField)frame.FindById("wnd[0]/tbar[0]/okcd")).Text = "/nztvc025";
+                frame.SendVKey(0);
+
+
+                int linhas1 = dataGridView1.RowCount;
+                int i;
+
+
+                ((GuiTextField)frame.FindById("wnd[0]/usr/ctxtP_PRFID")).Text = "0001";
+                ((GuiTextField)frame.FindById("wnd[0]/usr/ctxtP_WERKS")).Text = tb_plant.Text;
+
+
+                for (i = 0; i < linhas1 - 1; i++)
+                {
+                    try
+                    {
+                        ((GuiButton)frame.FindByName("btn[8]", "GuiButton")).Press();
+
+
+                        string material = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                        int tam = material.Length;
+                        string component1 = dataGridView1.Rows[i].Cells[1].Value.ToString();
+
+                        if (tam == 11)
+                        {
+                            ((GuiTextField)session.ActiveWindow.FindById("wnd[1]/usr/sub:SAPLSPO4:0300/ctxtSVALD-VALUE[1,21]")).Text = material;
+
+                        }
+
+                        else ((GuiTextField)session.ActiveWindow.FindById("wnd[1]/usr/sub:SAPLSPO4:0300/ctxtSVALD-VALUE[0,21]")).Text = material;
+
+                        ((GuiButton)frame.FindByName("btn[0]", "GuiButton")).Press();
+
+                        ((GuiToolbarControl)frame.FindById("wnd[0]/usr/subSUBSCR_TIPOS:SAPLZRVC_ZBOM_POS_ESP:0200/cntlCONT_TIPOS/shellcont/shell/shellcont[1]/shell[0]")).PressButton("&FIND");
+                        ((GuiTextField)session.ActiveWindow.FindById("wnd[1]/usr/txtLVC_S_SEA-STRING")).Text = tb_id.Text.ToString();
+                        ((GuiButton)frame.FindByName("btn[0]", "GuiButton")).Press();
+                        ((GuiButton)frame.FindByName("btn[0]", "GuiButton")).Press();
+
+                        if (session.Children.Count != 2)
+                        {
+                            dataGridView1.Rows[i].Cells[2].Value = "N/A";
+                            goto acabou;
+                        }
+
+                        ((GuiButton)frame.FindByName("btn[0]", "GuiButton")).Press();
+
+                        for (int j = 1; j < 100; j++)
+                        {
+
+                            string esp;
+                            if (j < 10) { esp = "          "; }
+                            else { esp = "         "; }
+
+                            ((GuiTree)frame.FindById("wnd[0]/usr/subSUBSCR_TIPOS:SAPLZRVC_ZBOM_POS_ESP:0200/cntlCONT_TIPOS/shellcont/shell/shellcont[1]/shell[1]")).SelectItem(esp + j, "&Hierarchy");
+                            ((GuiTree)frame.FindById("wnd[0]/usr/subSUBSCR_TIPOS:SAPLZRVC_ZBOM_POS_ESP:0200/cntlCONT_TIPOS/shellcont/shell/shellcont[1]/shell[1]")).EnsureVisibleHorizontalItem(esp + j, "&Hierarchy");
+                            ((GuiTree)frame.FindById("wnd[0]/usr/subSUBSCR_TIPOS:SAPLZRVC_ZBOM_POS_ESP:0200/cntlCONT_TIPOS/shellcont/shell/shellcont[1]/shell[1]")).DoubleClickItem(esp + j, "&Hierarchy");
+
+
+                            if (((GuiCTextField)session.ActiveWindow.FindByName("ZTBVC_657-ID_ITEM", "GuiCTextField")).Text == tb_id.Text.ToString())
+                            {
+                                ((GuiToolbarControl)frame.FindById("wnd[0]/usr/subSUBSCR_TIPOS:SAPLZRVC_ZBOM_POS_ESP:0200/cntlCONT_TIPOS/shellcont/shell/shellcont[1]/shell[0]")).PressButton("EDIT_ESP");
+                                ((GuiTextField)frame.FindById("wnd[0]/usr/subSUBSCR_ESPEC:SAPLZRVC_ZBOM_POS_ESP:0300/subSUBSCR_ESP:SAPLZRVC_ZBOM_POS_ESP:0340/ctxtZTBVC_658-IDNRK")).Text = component1;
+                                ((GuiButton)frame.FindByName("btn[8]", "GuiButton")).Press();
+                                dataGridView1.Rows[i].Cells[2].Value = "OK";
+                                break;
+                            }
+
+                        }
+
+
+
+                    acabou: ((GuiButton)frame.FindByName("btn[2]", "GuiButton")).Press();
+                    }
+                    catch
+                    {
+                        ((GuiButton)frame.FindByName("btn[2]", "GuiButton")).Press();
+                    }
+
+                    #endregion
+                }
+
+                MessageBox.Show("Alteração Concluída", "Alteração Concluída", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar com SAP", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+
+            }
+        }
+
+
+
+        private void dataGridView1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+
+            if ((e.Shift && e.KeyCode == Keys.Insert) || (e.Control && e.KeyCode == Keys.V))
+
+            {
+
+                char[] rowSplitter = { '\r', '\n' };
+
+                char[] columnSplitter = { '\t' };
+
+                //get the text from clipboard
+
+                IDataObject dataInClipboard = Clipboard.GetDataObject();
+
+                string stringInClipboard = (string)dataInClipboard.GetData(DataFormats.Text);
+
+                //split it into lines
+
+                string[] rowsInClipboard = stringInClipboard.Split(rowSplitter, StringSplitOptions.RemoveEmptyEntries);
+
+                //get the row and column of selected cell in grid
+
+                int r = dataGridView1.SelectedCells[0].RowIndex;
+
+                int c = dataGridView1.SelectedCells[0].ColumnIndex;
+
+                //add rows into grid to fit clipboard lines
+
+                if (dataGridView1.Rows.Count < (r + rowsInClipboard.Length))
+
+                {
+
+                    dataGridView1.Rows.Add(r + rowsInClipboard.Length - dataGridView1.Rows.Count + 1);
+
+                }
+
+                //loop through the lines, split them into cells and place the values in the corresponding cell.
+
+                for (int iRow = 0; iRow < rowsInClipboard.Length; iRow++)
+
+                {
+
+                    //split row into cell values
+
+                    string[] valuesInRow = rowsInClipboard[iRow].Split(columnSplitter);
+
+                    //cycle through cell values
+
+                    for (int iCol = 0; iCol < valuesInRow.Length; iCol++)
+
+                    {
+
+                        //assign cell value, only if it within columns of the grid
+
+                        if (dataGridView1.ColumnCount - 1 >= c + iCol)
+
+                        {
+
+                            dataGridView1.Rows[r + iRow].Cells[c + iCol].Value = valuesInRow[iCol];
+
+                        }
+
+                    }
+
+                }
+                label5.Text = (dataGridView1.Rows.Count - 1).ToString() + " " + "linhas";
+
+                if (dataGridView1.Rows.Count < 60)
+                {
+                    label6.Text = "Tempo Aprox. = " + ((dataGridView1.Rows.Count - 1) * 1).ToString() + " " + "segundos";
+                }
+                else
+                {
+                    label6.Text = "Tempo Aprox. = " + ((dataGridView1.Rows.Count - 1) * 0.016).ToString() + " " + "minutos";
+                }
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.RowCount == 1)
+            {
+                return;
+            }
+
+            SaveFileDialog salvar = new SaveFileDialog();
+            // novo SaveFileDialog
+
+
+            Excel.Application App; // Aplicação Excel
+            Excel.Workbook WorkBook; // Pasta
+            Excel.Worksheet WorkSheet; // Planilha
+            object misValue = System.Reflection.Missing.Value;
+
+            App = new Excel.Application();
+            WorkBook = App.Workbooks.Add(misValue);
+            WorkSheet = (Excel.Worksheet)WorkBook.Worksheets.get_Item(1);
+            int i = 0;
+            int j = 0;
+
+            // passa as celulas do DataGridView para a Pasta do Excel
+            for (i = 1; i < dataGridView1.ColumnCount + 1; i++)
+            {
+                WorkSheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+
+            for (i = 0; i <= dataGridView1.RowCount - 1; i++)
+            {
+                for (j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    DataGridViewCell cell = dataGridView1[j, i];
+                    WorkSheet.Cells[i + 2, j + 1] = cell.Value;
+                }
+            }
+
+            App.Visible = true;
+
+        }
+
+        private void btn_new_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            tb_id.Text = null;
+            tb_plant.Text = null;
+
+        }
     }
+
 }
